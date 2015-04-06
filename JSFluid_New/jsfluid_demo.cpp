cdd6ac139815e5baddb415799cfaf615b357e258
Davide Pasca <davide@oykgames.com>
2015-04-07 00:12:56 +0900

    Refactoring of demo source

--- JSFluid_New/jsfluid_demo.cpp
+++ JSFluid_New/jsfluid_demo.cpp
@@ -19,18 +19,31 @@ static float FORCE = 5.f;
 static float SOURCE_DENSITY = 100;
 static bool  DISPLAY_VEL = false;
 
-static std::vector<char>   gTmpBuff;
-static FluidSolver<N,true> gsSolver;
+static std::vector<char>   _tmpBuff;
+static FluidSolver<N,true> _solver;
 
-static int win_id;
-static int win_x, win_y;
-static int mouse_down[3];
-static int omx, omy, mx, my;
+//==================================================================
+struct Env
+{
+    int win_id = 0;
+    int win_x = 0;
+    int win_y = 0;
+    int mouse_down[3];
+    int omx = 0;
+    int omy = 0;
+    int mx  = 0;
+    int my  = 0;
+
+    Env()
+    {
+        for (auto &x : mouse_down) x = 0;
+    }
+} _env;
 
 //==================================================================
 static void pre_display()
 {
-	glViewport( 0, 0, win_x, win_y );
+	glViewport( 0, 0, _env.win_x, _env.win_y );
 	glMatrixMode( GL_PROJECTION );
 	glLoadIdentity();
 	gluOrtho2D( 0.0, 1.0, 0.0, 1.0 );
@@ -58,8 +71,8 @@ static void draw_velocity()
 
 				glVertex2f( x, y );
 				glVertex2f(
-                    x + gsSolver.SMPVel<0>( i, j ),
-                    y + gsSolver.SMPVel<1>( i, j ) );
+                    x + _solver.SMPVel<0>( i, j ),
+                    y + _solver.SMPVel<1>( i, j ) );
 			}
 		}
 
@@ -80,10 +93,10 @@ static void draw_density()
             {
 				float y = (j-0.5f)*h;
 
-				float d00 = gsSolver.SMPDen( i  , j   );
-				float d01 = gsSolver.SMPDen( i  , j+1 );
-				float d10 = gsSolver.SMPDen( i+1, j   );
-				float d11 = gsSolver.SMPDen( i+1, j+1 );
+				float d00 = _solver.SMPDen( i  , j   );
+				float d01 = _solver.SMPDen( i  , j+1 );
+				float d10 = _solver.SMPDen( i+1, j   );
+				float d11 = _solver.SMPDen( i+1, j+1 );
 
 				glColor3f( d00, d00, d00 ); glVertex2f( x, y );
 				glColor3f( d10, d10, d10 ); glVertex2f( x+h, y );
@@ -98,30 +111,28 @@ static void draw_density()
 //==================================================================
 static void get_from_UI()
 {
-	if ( !mouse_down[0] && !mouse_down[2] ) return;
+	if ( !_env.mouse_down[0] && !_env.mouse_down[2] ) return;
 
-	int i = (int)((       mx /(float)win_x)*N+1);
-	int j = (int)(((win_y-my)/(float)win_y)*N+1);
+	int i = (int)((            _env.mx /(float)_env.win_x)*N+1);
+	int j = (int)(((_env.win_y-_env.my)/(float)_env.win_y)*N+1);
 
 	if ( i<1 || i>N || j<1 || j>N ) return;
 
     const float dt = TIME_DELTA;
 
-	if ( mouse_down[0] )
+	if ( _env.mouse_down[0] )
     {
-		gsSolver.SMPVel<0>( i, j ) += FORCE * (mx-omx) * dt;
-		gsSolver.SMPVel<1>( i, j ) += FORCE * (omy-my) * dt;
+		_solver.SMPVel<0>( i, j ) += FORCE * (_env.mx  - _env.omx) * dt;
+		_solver.SMPVel<1>( i, j ) += FORCE * (_env.omy - _env.my) * dt;
 	}
 
-	if ( mouse_down[2] )
+	if ( _env.mouse_down[2] )
     {
-		gsSolver.SMPDen( i, j ) += SOURCE_DENSITY * dt;
+		_solver.SMPDen( i, j ) += SOURCE_DENSITY * dt;
 	}
 
-	omx = mx;
-	omy = my;
-
-	return;
+	_env.omx = _env.mx;
+	_env.omy = _env.my;
 }
 
 //==================================================================
@@ -131,7 +142,7 @@ static void key_func( unsigned char key, int x, int y )
 	{
 		case 'c':
 		case 'C':
-            gsSolver.Clear();
+            _solver.Clear();
 			break;
 
 		case 'q':
@@ -148,36 +159,36 @@ static void key_func( unsigned char key, int x, int y )
 
 static void mouse_func( int button, int state, int x, int y )
 {
-	omx = mx = x;
-	omx = my = y;
+	_env.omx = _env.mx = x;
+	_env.omx = _env.my = y;
 
-	mouse_down[button] = state == GLUT_DOWN;
+	_env.mouse_down[button] = (state == GLUT_DOWN);
 }
 
 static void motion_func( int x, int y )
 {
-	mx = x;
-	my = y;
+	_env.mx = x;
+	_env.my = y;
 }
 
 static void reshape_func( int width, int height )
 {
-	glutSetWindow( win_id );
+	glutSetWindow( _env.win_id );
 	glutReshapeWindow( width, height );
 
-	win_x = width;
-	win_y = height;
+	_env.win_x = width;
+	_env.win_y = height;
 }
 
 static void idle_func()
 {
 	get_from_UI();
 
-	gsSolver.vel_step( gTmpBuff.data(), VISCOSITY, TIME_DELTA );
+	_solver.vel_step( _tmpBuff.data(), VISCOSITY, TIME_DELTA );
 
-	gsSolver.dens_step( gTmpBuff.data(), DIFFUSION_RATE, TIME_DELTA );
+	_solver.dens_step( _tmpBuff.data(), DIFFUSION_RATE, TIME_DELTA );
 
-	glutSetWindow( win_id );
+	glutSetWindow( _env.win_id );
 	glutPostRedisplay();
 }
 
@@ -199,8 +210,8 @@ static void open_glut_window( void )
 	glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
 
 	glutInitWindowPosition( 200, 200 );
-	glutInitWindowSize( win_x, win_y );
-	win_id = glutCreateWindow( "Fluid Test" );
+	glutInitWindowSize( _env.win_x, _env.win_y );
+	_env.win_id = glutCreateWindow( "Fluid Test" );
 
 	glClearColor( 0.0f, 0.0f, 0.0f, 1.0f );
 	glClear( GL_COLOR_BUFFER_BIT );
@@ -259,11 +270,11 @@ int main( int argc, char ** argv )
 	printf( "\t Clear the simulation by pressing the 'c' key\n" );
 	printf( "\t Quit by pressing the 'q' key\n" );
 
-    gTmpBuff.resize( gsSolver.GetTempBuffMaxSize() );
-    //for (auto &x : gTmpBuff) x = 0;
+    _tmpBuff.resize( _solver.GetTempBuffMaxSize() );
+    //for (auto &x : _tmpBuff) x = 0;
 
-	win_x = 512;
-	win_y = 512;
+	_env.win_x = 512;
+	_env.win_y = 512;
 	open_glut_window();
 
 	glutMainLoop();
