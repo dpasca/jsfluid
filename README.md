ce5f832431381e3ad34ddf3cd8501401a4a6488c
Davide Pasca <davide@newtypekk.com>
2022-05-26 04:00:04 +0900

    Moved build to CMake

--- README.md
+++ README.md
@@ -6,9 +6,35 @@
 |---|---|
 | Author: | Davide Pasca |
 | Platform: | OpenGL + glut |
-| Build environment: | Visual Studio |
+| Build environment: | CMake |
 
 ## Overview
 
 So far this is simply a refactoring of the source code that came with Jos Stam's 2003 [paper](http://www.autodeskresearch.com/publications/games)
 
+## Required Tools
+- [Git](https://git-for-windows.github.io/) with Git Bash
+- [CMake](https://cmake.org/download/)
+- For Windows: Visual Studio
+- For Mac: Xcode
+- For Linux: gcc, etc.
+
+If you're on Windows. All commands are meant to run under Git Bash.
+
+## Quick Start
+
+### Do a full build
+```bash
+./build.sh
+```
+Executables can now be found under `_bin`.
+
+### Using the VS solution
+1. The solution is located in `_build\win\jsfluid.sln`
+2. From *Solution Explorer*, right click on **Demo1** and choose *"Set As StartUp Project"*
+3. Press F7 to build the selected project
+
+### Updating the VS solution or makefiles
+```bash
+./build.sh -d
+```
---
+++ build.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     MACHINE=linux;;
+    Darwin*)    MACHINE=macos;;
+    CYGWIN*)    MACHINE=win;;
+    MINGW*)     MACHINE=win;;
+    *)          echo "unsupported architecture"; exit 1
+esac
+
+BASHSCRIPTDIR="$(cd "$(dirname "$0")" || exit; pwd)"
+ROOTDIR=${BASHSCRIPTDIR}
+SOURCEDIR=${BASHSCRIPTDIR}
+BUILDDIR=${ROOTDIR}/_build/${MACHINE}
+SOURCEDIR=${ROOTDIR}/apps
+#SOURCE2DIR=${ROOTDIR}/libs
+NBFILES=${BUILDDIR}/.nbfiles
+
+displayusage() {
+    echo " ================================================================================= "
+    echo "|    Usage:                                                                       |"
+    echo "| do_build.sh OPTS                                                                |"
+    echo "|    available options [OPTS]:                                                    |"
+    echo "| -b) --build)          automatically updates the build when necessary            |"
+    echo "| -c) --clean)          removes build dirs                                        |"
+    echo "| -d) --dry-run)        creates the make file without building                    |"
+    echo "| -f) --force)          forces an update of the build                             |"
+    echo "| -h) --help)           print this help                                           |"
+    echo "| -m) --make)           performs make                                             |"
+	echo "| -n) --nproc)          sets the number of parallel processing (default nproc -1) |"
+    echo "| -t) --build-type)     specifies a different cmake build type (e.g. \"-t -Debug\") |"
+    echo "| -w) --cmake-params)   specifies cmake options in quoted (e.g. \"-DVAR=value\")    |"
+	echo "| [no arguments]        automatically updates the build when necessary            |"
+    echo " ================================================================================= "
+}
+
+update_makefiles(){
+    mkdir -p "${BUILDDIR}"
+    cd "${BUILDDIR}" || exit
+
+	if [ "${MACHINE}" == "macos" ]; then
+        cmake "${ROOTDIR}" -DCMAKE_BUILD_TYPE="${BUILDTYPE:-Release}" ${CMAKEOPTS:+$CMAKEOPTS}
+    elif [ "${MACHINE}" == "linux" ]; then
+        cmake "${ROOTDIR}" -DCMAKE_BUILD_TYPE="${BUILDTYPE:-Release}" ${CMAKEOPTS:+$CMAKEOPTS}
+    elif [ "${MACHINE}" == "win" ]; then
+        cmake -A Win32 "${ROOTDIR}" ${CMAKEOPTS:+$CMAKEOPTS}
+    fi
+    CMAKE_RET=$?
+	if [ $CMAKE_RET -ne 0 ] ; then exit ${CMAKE_RET}; fi
+    cd - || exit
+    MAKEFILEUPDATED="TRUE"
+}
+
+build(){
+    if [[ ! -f "${NBFILES}" ]] ; then mkdir -p "${BUILDDIR}";  echo 0 > "${NBFILES}" ; fi
+    PREVFILES=$(<"${NBFILES}")
+    CURRFILES1=$(find "${SOURCEDIR}" |  wc -l)
+    #CURRFILES2=$(find "${SOURCE2DIR}" |  wc -l)
+    CURRFILES=$((CURRFILES1 + CURRFILES2))
+
+    if [[ "${MAKEFILEUPDATED}" == "TRUE" ]] || [[ "${CURRFILES}" -ne "${PREVFILES}" ]] ; then update_makefiles ; fi
+
+	if [ "${MACHINE}" == "win" ]; then
+		cmake --build "${BUILDDIR}" --config "${BUILDTYPE:-Release}" -- -m:"${NPROC}"
+	else
+		cmake --build "${BUILDDIR}" --config "${BUILDTYPE:-Release}" -j "${NPROC}"
+	fi
+    CMAKE_RET=$?
+    echo "${CURRFILES}" > "${NBFILES}"
+	if [ $CMAKE_RET -ne 0 ] ; then exit ${CMAKE_RET}; fi
+}
+
+emake(){
+    cd "${BUILDDIR}" || exit ; make -j"${NPROC}"
+}
+
+for arg in "$@"; do
+	shift
+	case "$arg" in
+		"--build")          set -- "$@" "-b" ;;
+		"--clean")          set -- "$@" "-c" ;;
+		"--dry-run")        set -- "$@" "-d" ;;
+		"--force")          set -- "$@" "-f" ;;
+		"--help")           set -- "$@" "-h" ;;
+		"--make")           set -- "$@" "-m" ;;
+		"--nproc")          set -- "$@" "-n" ;;
+		"--build-type")     set -- "$@" "-t" ;;
+		"--cmake-params")   set -- "$@" "-w" ;;
+		*)                  set -- "$@" "$arg";;
+	esac
+done
+
+# Parse short options
+OPTIND=1
+while getopts "bcdfhilmn:pqrt:w:?" opt
+do
+	case "$opt" in
+		"b") build; exit 0;;
+		"c") CLEANBUILD="TRUE";;
+		"d") UPDATEMAKEFILES="TRUE"; DRY_RUN="TRUE" ;;
+		"f") UPDATEMAKEFILES="TRUE";;
+		"h") displayusage; exit 0;;
+		"m") emake; exit 0;;
+		"n") NPROC=${OPTARG};;
+		"t") BUILDTYPE=${OPTARG}; UPDATEMAKEFILES="TRUE";;
+		"w") CMAKEOPTS+="${OPTARG} "; UPDATEMAKEFILES="TRUE";;
+        "?") displayusage; exit 0;;
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+if [[ -z "${NPROC}" ]]; then
+	if [ "${MACHINE}" == "win" ]; then
+		(( NPROC = 3 ))
+	else
+		(( NPROC = $(nproc) - 1 ))
+	fi
+fi
+
+
+if [[ "${CLEANBUILD}" == "TRUE" ]] ; then rm -rf _bin _build; fi
+
+if [[ "${UPDATEMAKEFILES}" == "TRUE" ]] ; then update_makefiles; fi
+
+if [[ "${DRY_RUN}" == "TRUE" ]] ; then  exit 0; fi
+
+build
+
